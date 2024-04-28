e747214fba26f71a5e30e5266d5121e4668b3ae5
Diego Carpio <u201714271@upc.edu.pe>
2024-04-27 23:12:42 -0500

    V6

---
+++ Aplicacion/Compra/PaginacionCurso.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicacion.Compra
+{
+    public class PaginacionCurso
+    {
+
+    }
+}
--- Dominio/Tablas/Producto.cs
+++ Dominio/Tablas/Producto.cs
@@ -10,13 +10,13 @@ namespace Dominio.Tablas
         public Guid Productoid { get; set; }
         public string Nombre { get; set; }
         [Column(TypeName = "decimal(18,4)")]
-        public decimal Precio { get; set; }
+        public decimal Precio { get; set; }        
         public string Categoria { get; set; }
         public byte[] Imagen { get; set; }
         public DateTime FechaCreacion { get; set; }
         public ICollection<ProductoCompra> CompraLink { get; set; }
         public ICollection<ProductoVenta> VentaLink { get; set; }
         public ICollection<ProductoPronosticoDemanda> PronosticoDemandaLink {  get; set; }
-        public ICollection<Favoritos> ListaFavoritos {  get; set; }
+        public Favoritos Favoritos { get; set; }
     }
 }
