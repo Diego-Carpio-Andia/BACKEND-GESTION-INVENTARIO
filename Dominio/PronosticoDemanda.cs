0f6e018b3f0291f811637396f3b9ef98970ba2a2
Diego Carpio <u201714271@upc.edu.pe>
2024-06-02 21:38:55 -0500

    v15

--- Dominio/PronosticoDemanda.cs
+++ Dominio/PronosticoDemanda.cs
@@ -8,6 +8,7 @@ namespace Dominio
     {
         public Guid PronosticoDemandaId { get; set; }
         public int CantidadPronosticada { get; set; }
+        public Usuario Usuario { get; set; }
         public DateTime FechaCreacion { get; set; }
         public ICollection<ProductoPronosticoDemanda> ProductoLink { get; set; }
         public Notificacion Notificacion { get; set; }
